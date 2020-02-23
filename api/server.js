325c94e80ffe1fe9380a6ddcdd3509560af0196f
silvodesigns <kevsilvo@gmail.com>
2020-02-23 03:54:17 -0500

    server is alive

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const server = express();
+
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.send('Its alive');
+})
+
+
+module.exports = server;
